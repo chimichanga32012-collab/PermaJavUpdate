92eb1f4db3f0b6efbe4e6f4d7031872948141e17
chimichanga32012-collab <chimichanga32012@gmail.com>
2026-06-23 15:53:37 -0300

    Add files via upload

---
+++ main/src/main/java/tech/sebazcrc/permadeath/util/log/PDCLog.java
@@ -0,0 +1,67 @@
+package tech.sebazcrc.permadeath.util.log;
+
+import org.bukkit.Bukkit;
+import tech.sebazcrc.permadeath.Main;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class PDCLog {
+
+    private static PDCLog logs;
+    private final Main instance;
+    private final File file;
+
+    public PDCLog() {
+        this.instance = Main.getInstance();
+        this.file = new File(instance.getDataFolder(), "logs.txt");
+
+        if (!instance.getDataFolder().exists()) {
+            instance.getDataFolder().mkdirs();
+        }
+
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void disable(String reason) {
+        log("El plugin ha sido apagado: " + reason);
+    }
+
+    public void log(String log) {
+        log(log, false);
+    }
+
+    public void log(String log, boolean print) {
+        LocalDate date = LocalDate.now();
+        LocalDateTime time = LocalDateTime.now();
+        String message = String.format("[%02d/%02d/%02d] ", date.getDayOfMonth(), date.getMonthValue(), date.getYear()) + String.format("%02d:%02d:%02d ", time.getHour(), time.getMinute(), time.getSecond()) + log + "\n";
+
+        add(message);
+        if (print) {
+            Bukkit.getConsoleSender().sendMessage(message);
+        }
+    }
+
+    private void add(String msg) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(file, true))) {
+            bw.append(msg);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static PDCLog getInstance() {
+        if (logs == null) logs = new PDCLog();
+        return logs;
+    }
+}
